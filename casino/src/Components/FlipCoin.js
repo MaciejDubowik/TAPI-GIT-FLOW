4c2779df4e4fc7e751182c1fe39d0d08b736928e
MaciejDubowik <maciejxpoland@gmail.com>
2023-12-15 11:20:05 +0100

    minor changes

--- casino/src/Components/FlipCoin.js
+++ casino/src/Components/FlipCoin.js
@@ -3,7 +3,7 @@ import React from "react";
 const FlipCoin = () => {
     return (
         <div>
-        <h1>FlipCoin</h1>
+        <h1>FlipCoin!!!!!!!!!!!!!!!!</h1>
         <p>FlipCoin page content</p>
         </div>
     );
